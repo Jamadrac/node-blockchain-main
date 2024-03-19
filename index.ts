1df791779cbb8752f75cb5485ba629d6d58677bd
jamadrac <rabbikindalo001@gmail.com>
2024-03-19 09:56:53 +0200

    as

--- index.ts
+++ index.ts
@@ -86,6 +86,33 @@ class Chain {
   }
 }
 
+// Wallet gives a user a public/private keypair
+class Wallet {
+  public publicKey: string;
+  public privateKey: string;
+
+  constructor() {
+    const keypair = crypto.generateKeyPairSync('rsa', {
+      modulusLength: 2048,
+      publicKeyEncoding: { type: 'spki', format: 'pem' },
+      privateKeyEncoding: { type: 'pkcs8', format: 'pem' },
+    });
+
+    this.privateKey = keypair.privateKey;
+    this.publicKey = keypair.publicKey;
+  }
+
+  sendMoney(amount: number, payeePublicKey: string) {
+    const transaction = new Transaction(amount, this.publicKey, payeePublicKey);
+
+    const sign = crypto.createSign('SHA256');
+    sign.update(transaction.toString()).end();
+
+    const signature = sign.sign(this.privateKey);
+    Chain.instance.addBlock(transaction, this.publicKey, signature);
+  }
+}
+
 // Setup Express.js server
 const app = express();
 const port = 3000; // Choose your desired port
